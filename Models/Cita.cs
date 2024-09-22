1e417b84017ca9aa541f71ade293169cfd8e66b3
RealCodeStream <github2023JP@gmail.com>
2024-09-21 22:10:48 -0500

    Solicitud, Historial y ViewModel listo

--- Models/Cita.cs
+++ Models/Cita.cs
@@ -13,19 +13,5 @@ namespace MedicalUTP.Models
         public required string MotivoConsulta { get; set; }
         public bool EsFutura => FechaHora > DateTime.Now;
     }
-    //{
-    //    [Key]
-    //    public int IdCita { get; set; }
-    //    public required int UsuarioId { get; set; }
-    //    [ForeignKey("IdUser")]
-    //    public required User User { get; set; }
-    //   // public required virtual User Refuser { get; set; }
-    //    public required string TipoConsulta { get; set; }
-    //    public DateTime FechaHora { get; set; }
-    //    public required string MedicoAsignado { get; set; }
-    //    public required string MotivoConsulta { get; set; }
-
-
-    //}
 
 }
